93463df55e596ef9f0c7d508349f5ce7ec5032de
Bruna Seewald <bruna.seewald@gmail.com>
2017-04-07 03:10:59 -0300

    HOME quase finalizada.

--- src/js/components/components/footer.js
+++ src/js/components/components/footer.js
@@ -6,10 +6,15 @@ class Footer extends Component {
         return (
             <div id="c-footer">
                 <div id="gradient-footer">
+					          <div className="store">
+						            <p id="footer-text">Download:</p>
+                        <a href="http://www.apple.com/br/" target="_blank"> <img className="apple" src="images/app-store-logo.png"/></a>
+					              <a href="https://play.google.com/store?hl=pt_BR" target="_blank"> <img className="googleplay" src="images/en_badge_web_generic.png"/></a>
+                	  </div>
                     <div className="row">
                         @2017 Freelas to Founders - Todos os direitos reservados
                     </div>
-                    <a href="http://www.fastdezine.com" target="_blank"> <img className="facebook" src="https://www.sanpedrovalley.org/wp-content/uploads/job-manager-uploads/featured_image/2016/05/fast.jpg"/></a>
+                    <a href="http://www.fastdezine.com" target="_blank"> <img className="fastdezine" src="images/fast2.jpg"/></a>
                 </div>
             </div>
         );
